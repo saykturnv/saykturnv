8e9dd8fc9ed173bb06aee6fe3914499fa9dd0901
student1 <student1@class.local>
2026-09-11 09:34:03 +0900

    Add requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World");
+{
+    "Print to console": {
+        "prefix": "main",
+        "body": [
+            "if __name__ == '__main__':",
+            "    $0"
+        ],
+        "description": "Create main block"
+    }
+}
